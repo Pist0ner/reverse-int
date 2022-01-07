4891bcc617f141b404faab2cd147563175b59192
Николай Малыгин <nikolos.f1@gmail.com>
2022-01-07 19:35:07 +0500

    fix: function reverse

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 module.exports = function reverse (n) {
     // function getReversedNum(num) {
-        let result = 0;
-        while (n) {
-            result = result * 10 + n % 10;
-            n = Math.floor(n / 10);
-        }
-
-        return result;
+    //     let result = 0;
+    //     while (n) {
+    //         result = result * 10 + n % 10;
+    //         n = Math.floor(n / 10);
+    //     }
+    //
+    //     return result;
     // }
+    let y = 0;
+    n = Math.abs(n)
+    for(; n; n = Math.floor(n / 10)) {
+        y *= 10;
+        y += n % 10;
+    }
+    return y
 }
